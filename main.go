9b78c66e644cf375938221a94aab37c652c0fad2
Setiyono Ressly <setiyono.ressly@gmail.com>
2023-11-15 12:44:25 +0700

    configure server

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 	"go-restful-api/app"
 	"go-restful-api/controller"
+	"go-restful-api/helper"
 	"go-restful-api/repository"
 	"go-restful-api/service"
+	"net/http"
 )
 
 func main() {
@@ -21,5 +24,10 @@ func main() {
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 }
